3ce792ac5cdec4f00689ed2c8cfe8a3c07c50f47
LuanRodrigues <47065448+LuanExcel@users.noreply.github.com>
2025-04-08 07:52:12 -0300

    update

---
+++ anotacoes.md
@@ -0,0 +1,4 @@
+# Paginas
+Importante colocar o numero na frente pra indicar a ordem a qual vai ficar
+
+Sempre q colocar uma session numa segunda ou terceira pagina sempre tem q colocar primeio a home
---
+++ pages/2_🏃‍♀️_players.py
@@ -0,0 +1,48 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Players",
+    page_icon="🏃‍♀️",
+    layout="wide"
+
+)
+
+
+df_data = st.session_state['data']
+
+clubes = df_data['Club'].value_counts().index
+club = st.sidebar.selectbox('Clube', clubes)
+
+df_players = df_data[df_data['Club'] == club]
+players = df_players['Name'].value_counts().index
+player = st.sidebar.selectbox('Jogador', players)
+
+player_stats = df_data[df_data['Name'] == player].iloc[0]
+
+st.image(player_stats['Photo'])
+st.title(player_stats['Name'])
+
+st.markdown(f"**Clube:** {player_stats['Club']}")
+st.markdown(f"**Posição:** {player_stats['Position']}")
+
+col1, col2, col3, col4 = st.columns(4)
+col1.markdown(f"**Idade:** {player_stats['Age']}")
+col2.markdown(f"**Altura:** {player_stats['Height(cm.)'] / 100}")
+col3.markdown(f"**Peso:** {player_stats['Weight(lbs.)']*0.453:.2f}")
+# Linha pra dividir a tela
+st.divider()
+
+st.subheader(f'Overall {player_stats['Overall']}')
+st.progress(int(player_stats['Overall']))
+
+# USnaod o with para deixar na horizontal
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.metric("Valor de mercado", f"£ {player_stats['Value(£)']:,}")
+
+with col2:
+    st.metric("Remuneração semanal", f"£ {player_stats['Wage(£)']:,}")
+
+with col3:
+    st.metric("Cláusula de rescisão", f"£ {player_stats['Release Clause(£)']:,}")
+
